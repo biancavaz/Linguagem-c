4710e57ef7aaaf1db1cf20e3c0ac900fcb973cc1
biancavaz <147559714+biancavaz@users.noreply.github.com>
2024-08-07 14:05:58 -0300

    07.08

---
+++ atividadesStruct/exemplo2Struct.c
@@ -0,0 +1,33 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main () {
+
+    float d;
+
+    struct ponto{
+        int x;
+        int y;
+    } p1, p2, p3, p4;
+
+    printf("\n--- Ponto 1 ---");
+    printf("\n--- cordenada x:");
+    scanf("%d", &p1.x);
+
+    printf("\n--- Ponto 2---");
+    printf("\n--- cordenada y:");
+    scanf("%d", &p2.x);
+
+    printf("\n--- Ponto 3---");
+    printf("\n--- cordenada y:");
+    scanf("%d", &p1.y);
+
+    printf("\n--- Ponto 4---");
+    printf("\n--- cordenada y:");
+    scanf("%d", &p2.y);
+
+    d = sqrt(pow ((p2.x - p1.x),2) + pow ((p2.y - p1.y), 2));
+    printf("\n Distancia entre os pontos: %.2f\n\n", d);
+}
